525c2a28a4bae30a7c74a018402f9ed6c5e1269f
TengJiang <1937419351@qq.com>
2020-06-17 18:58:27 +0800

    rule13

---
+++ effectiveJava/src/com/mrpicker/chapter3/Rule13.java
@@ -0,0 +1,15 @@
+package com.mrpicker.chapter3;
+
+import com.mrpicker.base.BaseRule;
+
+/**
+ * @program: effectiveJava
+ * @description: 谨慎地覆盖clone
+ * @author: Mrpicker
+ * @create: 2020-06-17 15:55
+ **/
+public class Rule13 extends BaseRule {
+    public Rule13(){
+        
+    }
+}
